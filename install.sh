c5907d29dcc4bbea3fb798189a66e08aeb643d6c
Kui <xukui.cs@gmail.com>
2019-11-18 22:22:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,13 @@ sudo python oh-my-tuna.py --global
 # zsh
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+zsh
 
 # tmux 
 sudo apt install tmux
 wget https://raw.githubusercontent.com/kuixu/xbash/master/.tmux.conf -O ~/.tmux.conf
+tmux 
+tmux source ~/.tmux.conf
+
+# htop 
+sudo apt install htop
