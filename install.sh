9eabec131cdd6a7f9f603dceba3af779f82fed0e
Kui <xukui.cs@gmail.com>
2019-11-26 17:33:07 +0800

    set zsh as default shell

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt update
 # zsh
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "zsh" >> ~/.bashrc
 zsh
 
 # tmux 
